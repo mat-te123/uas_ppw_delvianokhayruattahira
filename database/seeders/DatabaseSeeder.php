7c25a2641a77ff2c464841f1d00db7d167746eba
mat-te123 <denodelviano@gmail.com>
2025-12-17 16:42:27 +0700

    implementasi soft delete pada table pekerjaan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Administrator',
-            'email' => 'admin@mail.com',
+            'email' => fake()->unique()->safeEmail(),
         ]);
     }
 }
