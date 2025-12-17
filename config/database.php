1a0d3c5e90b3d11960a6c3bbdb3480d19e868bf0
mat-te123 <denodelviano@gmail.com>
2025-12-17 16:09:21 +0700

    mengganti driver dari sqllite ke mysql

--- config/database.php
+++ config/database.php
@@ -32,10 +32,10 @@ return [
     'connections' => [
 
         'sqlite' => [
-            'driver' => 'sqlite',
+            'driver' => 'mysql',
             'url' => env('DB_URL'),
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => 'DelvianoKhayru_543732',
+            'prefix' => 'DelvianoKhayru_543732_',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
             'busy_timeout' => null,
             'journal_mode' => null,
